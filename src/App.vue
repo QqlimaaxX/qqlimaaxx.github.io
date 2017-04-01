2f7c8b8c38902c96a0c4b8535e0028d42da7855b
QqlimaaxX <mohitkarangiya@gmail.com>
2017-04-01 14:14:03 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <todo-list :todos="todos"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+
+export default {
+  name: 'app',
+  components: {
+    TodoList
+  },
+  data(){
+    return{
+      todos:[
+        {
+        title:'Complete Math Assignment',
+        desc:'Submission on Monday shit',
+        done:false
+        },{
+        title:'CSA Assignment also',
+        desc:'ON Monday',
+        done:false
+        },{
+        title:'Get Lab Manuals Checked',
+        desc:'DO IT FAST IDIOT',
+        done:false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
