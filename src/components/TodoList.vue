07786b9415be441d03d34dd5ecec014c687e413e
QqlimaaxX <mohitkarangiya@gmail.com>
2017-04-01 14:13:12 +0530

    Create TodoList.vue

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+	<div class="ui center aligned segment">
+		<h3>Completed Tasks - {{todos.filter(todo=>todo.done).length}}</h3>
+		<h3>Pending Tasks - {{todos.filter(todo=>!todo.done).length}}</h3>
+	</div>
+	<todo v-for='todo,index in todos' :todo="todo" ></todo>
+	</div>
+</template>
+
+<script>
+
+import Todo from './Todo'
+
+	export default{
+		props:["todos"],
+		components:{Todo}
+	};
+</script>
+
+<style>
+	
+</style>
